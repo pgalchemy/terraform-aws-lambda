d9b6d3ffe28d5722a7d98e5d69e6709e87b58711
Joshua Rose <joshuarose@outlook.com>
2019-11-26 13:47:33 -0500

    Misc changes in module

--- modules/event-trigger/apigw/variables.tf
+++ modules/event-trigger/apigw/variables.tf
@@ -5,37 +5,37 @@ variable "enable" {
 }
 
 variable "name" {
-  type        = "string"
+  type        = string
   description = "Name of the REST API"
 }
 
 variable "stage_name" {
-  type        = "string"
+  type        = string
   description = "Stage name for REST api"
 }
 
 variable "rest_method" {
-  type        = "string"
+  type        = string
   description = "HTTP method for the endpoint"
 }
 
 variable "path_part" {
-  type        = "string"
+  type        = string
   description = "Unique piece of path for this APIGW endpoint"
 }
 
 variable "api_version" {
-  type        = "string"
+  type        = string
   description = "Current version of API"
   default     = "v1"
 }
 
 variable "function_name" {
-  type        = "string"
+  type        = string
   description = "Function name of the related Lambda"
 }
 
 variable "invoke_arn" {
-  type        = "string"
+  type        = string
   description = "Invoke arn of the related Lambda"
 }
