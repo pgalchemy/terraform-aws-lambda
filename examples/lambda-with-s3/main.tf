638d880e32b92b41788cc1d4d374a5a177f7c62e
brookesargent <brookesargent01@gmail.com>
2020-07-16 16:46:24 -0400

    finish up lambda s3 example so example can be run without existing s3 infrastructure

--- examples/lambda-with-s3/main.tf
+++ examples/lambda-with-s3/main.tf
@@ -7,18 +7,34 @@ provider aws {
   version = ">= 2.35.0"
 }
 
+locals {
+  bucket_name = "terratest-lambda-bucket"
+  bucket_key  = "hello_world.zip"
+}
+
+
+resource aws_s3_bucket bucket {
+  bucket = local.bucket_name
+}
+
+resource aws_s3_bucket_object key {
+  bucket = aws_s3_bucket.bucket.bucket
+  key    = local.bucket_key
+  source = local.bucket_key
+}
+
 module lambda {
   source = "../../"
 
   lambda_name                    = "terratest"
   handler                        = "terratest"
   lambda_description             = "Lambda function to test terraform-aws-lambda module"
-  s3_bucket                      = "s3-binaries-572156681732-us-east-1"
-  s3_key                         = "bin/provision.zip"
+  s3_bucket                      = local.bucket_name
+  s3_key                         = aws_s3_bucket_object.key.key
   region                         = "us-east-1"
   rest_api_id                    = ""
   version_id                     = ""
-  publish                        = true
+  publish                        = false
   reserved_concurrent_executions = 2
   environment = {
     variables = {
